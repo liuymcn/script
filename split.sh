d9bd6650810a7acc2422b2a52498f027f4caf06b
liuymcn <liuym.1225836327@gmail.com>
2018-07-30 12:17:49 +0800

    Add files via upload

---
+++ split.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+userage(){
+echo -e "Usage: 
+  命令使用格式：
+    sh scriptName spl number（按多少行进行分割）fileName（需要分割的源文件） afterName（分割后的文件前缀）
+  如：sh chaifen.sh split 10000 offer_affiliate_cap_conf_ins_1953.sql offer_affiliate_
+"
+}
+
+#文件拆分函数，根据传入参数进行拆分
+spl()
+{
+filelines=$1
+fileName=$2
+zifile=$3
+split -l $1  $2  -d -a 5  $3
+for i in $3*
+    do mv $i $i.sql
+done
+}
+
+if [ "$1" = "split" ]&& [ -n  "$2" ]&& [ -n  "$3" ]&& [ -n  "$4" ];
+then
+    spl $2 $3 $4;
+if [ "$1" = "userage" ] || [ "$1" = "help" ];
+then
+   userage
+fi
+else
+   userage
+   exit
+fi
